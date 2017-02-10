91808c49a8030f513617ca28247f0e728eeb4073
Eitaro Fukamachi <e.arrows@gmail.com>
2017-02-10 15:18:30 +0900

    Add serial::windows::open_async.

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -3,3 +3,9 @@ pub use self::com::*;
 mod com;
 mod error;
 mod ffi;
+
+use std::ffi::OsStr;
+
+pub fn open_async<T: AsRef<OsStr> + ?Sized>(port: &T) -> ::Result<COMPort> {
+    COMPort::open_async(port)
+}
